38652978af9ec6eab238181419b69866719eeb3b
Sarah Kohl <sarahcharlottekohl@gmail.com>
2017-02-02 20:54:43 -0800

    Skeleton React project

---
+++ app/components/App.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Welcome = React.createClass({
+  render: function(){
+    return <h1>This website is under construction!</h1>;
+  }
+});
+
+module.exports = Welcome;
